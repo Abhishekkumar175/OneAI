2aee9d8d503be5a0c9288d2e9e888db216a4a30f
abhishekkumar <abhishekkumar345ssm@gmail.com>
2026-01-12 20:01:02 +0530

    Home page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
